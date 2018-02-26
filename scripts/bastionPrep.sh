90124838bb410a91e930836b5f5a73469404f31a
Mike Savage <mike.savage@gmail.com>
2018-02-25 23:28:20 -0500

    Add docker to bastion host

--- scripts/bastionPrep.sh
+++ scripts/bastionPrep.sh
@@ -71,6 +71,7 @@ subscription-manager repos \
 echo $(date) " - Install base packages and update system to latest packages"
 
 yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion httpd-tools kexec-tools sos psacct
+yum -y install cloud-utils-growpart.noarch
 yum -y update --exclude=WALinuxAgent
 yum -y install atomic-openshift-excluder atomic-openshift-docker-excluder
 atomic-openshift-excluder unexclude
@@ -79,6 +80,45 @@ atomic-openshift-excluder unexclude
 echo $(date) " - Installing OpenShift utilities"
 
 yum -y install atomic-openshift-utils
+
+# Grow Root File System
+echo $(date) " - Grow Root FS"
+
+rootdev=`findmnt --target / -o SOURCE -n`
+rootdrivename=`lsblk -no pkname $rootdev`
+rootdrive="/dev/"$rootdrivename
+majorminor=`lsblk  $rootdev -o MAJ:MIN | tail -1`
+part_number=${majorminor#*:}
+
+growpart $rootdrive $part_number -u on
+xfs_growfs $rootdev
+
+# Install Docker 1.12.x
+echo $(date) " - Installing Docker 1.12.x"
+
+yum -y install docker
+sed -i -e "s#^OPTIONS='--selinux-enabled'#OPTIONS='--selinux-enabled --insecure-registry 172.30.0.0/16'#" /etc/sysconfig/docker
+
+# Create thin pool logical volume for Docker
+echo $(date) " - Creating thin pool logical volume for Docker and staring service"
+
+DOCKERVG=$( parted -m /dev/sda print all 2>/dev/null | grep unknown | grep /dev/sd | cut -d':' -f1  | awk 'NR==1' )
+
+echo "DEVS=${DOCKERVG}" >> /etc/sysconfig/docker-storage-setup
+echo "VG=docker-vg" >> /etc/sysconfig/docker-storage-setup
+docker-storage-setup
+if [ $? -eq 0 ]
+then
+   echo "Docker thin pool logical volume created successfully"
+else
+   echo "Error creating logical volume for Docker"
+   exit 5
+fi
+
+# Enable and start Docker services
+
+systemctl enable docker
+systemctl start docker
 
 # Create playbook to update ansible.cfg file to include path to library
 # Filename: updateansiblecfg.yaml
