0843ba947e727975d7662be34372cf61ce3b872d
Mike Savage <mike.savage@gmail.com>
2018-05-21 00:57:54 -0400

    Add repo

--- scripts/glusterPrep.sh
+++ scripts/glusterPrep.sh
@@ -68,6 +68,7 @@ subscription-manager repos --disable="*"
 subscription-manager repos \
     --enable="rhel-7-server-rpms" \
     --enable="rhel-7-server-extras-rpms" \
+    --enable="rhel-7-server-ose-3.9-rpms" \
     --enable="rhel-7-server-ansible-2.4-rpms" \
     --enable="rhel-7-fast-datapath-rpms"
 
